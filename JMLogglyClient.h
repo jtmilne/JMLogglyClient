a0ee5b32d0404789afa0cf41e4985df4d38fcf25
Joel Milne <joel.milne@gmail.com>
2018-04-24 10:39:08 -0700

    Initial commit

---
+++ JMLogglyClient.h
@@ -0,0 +1,13 @@
+//
+//  JMLogglyClient.h
+//  WordsWithCupid
+//
+//  Created by Joel Milne on 4/24/18.
+//  Copyright © 2018 Games With Cupid. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface JMLogglyClient : NSObject
+
+@end
---
+++ JMLogglyClient.m
@@ -0,0 +1,13 @@
+//
+//  JMLogglyClient.m
+//  WordsWithCupid
+//
+//  Created by Joel Milne on 4/24/18.
+//  Copyright © 2018 Games With Cupid. All rights reserved.
+//
+
+#import "JMLogglyClient.h"
+
+@implementation JMLogglyClient
+
+@end
